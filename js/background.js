024e1ada39aebd8334803aa81a285fa392b40ac3
stellum <stellaloh16@gmail.com>
2022-09-12 03:03:56 +0900

    Fetch Unsplash API

--- js/background.js
+++ js/background.js
@@ -1,10 +1,25 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+// const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+// const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+// const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+// bgImage.src = `img/${chosenImage}`;
 
-// appendChild 하는일? add HTML to the <body>
-document.body.appendChild(bgImage);
+// // appendChild 하는일? add HTML to the <body>
+// document.body.appendChild(bgImage);
+
+const imageWidth = 1970;
+const imageHeight = 1080;
+const collectionID = 8469893;
+
+fetch(
+  `https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/`
+)
+  .then((response) => {
+    const jsBody = document.querySelector("body");
+    jsBody.style = `background-image: url('${response.url}')`;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
